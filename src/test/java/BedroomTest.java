84673c3d775df71be41e70f5934fb1e318018f5c
codeonium <codeon2020@gmail.com>
2021-05-05 11:48:37 +0100

    Initial Commit

---
+++ src/test/java/BedroomTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BedroomTest {
+
+    private Bedroom bedroom;
+    private Guest guest;
+
+    @Before
+    public void setUp() {
+        bedroom = new Bedroom(12, 2, "Double");
+        guest = new Guest("Malcolm");
+    }
+
+    @Test
+    public void hasRoomNumber() {
+        assertEquals(12, bedroom.getRoomNumber());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(2, bedroom.getCapacity());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("Double", bedroom.getType());
+    }
+
+    @Test
+    public void guestsStartsEmpty() {
+        assertEquals(0, bedroom.guestCount());
+    }
+
+    @Test
+    public void canHaveGuests() {
+        bedroom.addGuest(guest);
+        assertEquals(1, bedroom.guestCount());
+    }
+
+
+
+}
